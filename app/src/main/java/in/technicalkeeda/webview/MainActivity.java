51803d107f58ef1b10b57e4c3e07a66c789f3fca
Mahavir Singh <bishtmanish739@gmail.com>
2019-11-26 11:54:31 +0530

    this is initail commit

---
+++ app/src/main/java/in/technicalkeeda/webview/MainActivity.java
@@ -0,0 +1,36 @@
+package in.technicalkeeda.webview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+public class MainActivity extends AppCompatActivity {
+    WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView=findViewById(R.id.webview);
+        webView.setWebViewClient(new WebViewClient());
+
+        webView.loadUrl("https://news.google.com/?hl=en-IN&gl=IN&ceid=IN:en");
+        WebSettings webSettings=webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+
+    }
+
+    @Override
+    public void onBackPressed() {
+       if(webView.canGoBack()){
+           webView.goBack();
+       }
+       else{
+           super.onBackPressed();
+       }
+    }
+}
