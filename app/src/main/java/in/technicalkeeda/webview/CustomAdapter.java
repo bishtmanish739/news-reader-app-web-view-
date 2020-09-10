40034b9d32529661bd85d3320a95a7bb35f6cf20
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-09-10 15:53:23 +0530

    update

---
+++ app/src/main/java/in/technicalkeeda/webview/CustomAdapter.java
@@ -0,0 +1,119 @@
+package in.technicalkeeda.webview;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class CustomAdapter extends BaseAdapter {
+    Context context;
+    int logos[];
+    LayoutInflater inflter;
+    public CustomAdapter(Context applicationContext, int[] logos) {
+        this.context = applicationContext;
+        this.logos = logos;
+        inflter = (LayoutInflater.from(applicationContext));
+    }
+    @Override
+    public int getCount() {
+        return logos.length;
+    }
+    @Override
+    public Object getItem(int i) {
+        return null;
+    }
+    @Override
+    public long getItemId(int i) {
+        return 0;
+    }
+    @Override
+    public View getView(int i, View view, ViewGroup viewGroup) {
+        view = inflter.inflate(R.layout.gridview, null); // inflate the layout
+        ImageView icon = (ImageView) view.findViewById(R.id.icon); // get the reference of ImageView
+        icon.setImageResource(logos[i]);
+        if(i==0){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.uttarakhand);
+            // set logo images
+        }
+        if(i==1){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.almora);
+            // set logo images
+        }
+        if(i==2){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.bageshwar);
+            // set logo images
+        }
+        if(i==3){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.chamoli);
+            // set logo images
+        }
+        if(i==4){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.champawat);
+            // set logo images
+        } if(i==5){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.dehradun);
+            // set logo images
+        }
+        if(i==6){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.haridwar);
+            // set logo images
+        } if(i==7){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.nainital);
+            // set logo images
+        } if(i==8){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.paurigarhwal);
+            // set logo images
+        } if(i==9){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.pithoragarh);
+            // set logo images
+        } if(i==10){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.rudraprayag);
+            // set logo images
+        }
+        if(i==11){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.tehrigarhwal);
+            // set logo images
+        } if(i==12){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.udhamsinghnagar);
+            // set logo images
+        }
+        if(i==13){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.uttarkash);
+            // set logo images
+        }
+        if(i==14){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.india);
+            // set logo images
+        }
+        if(i==15){
+            TextView title = (TextView) view.findViewById(R.id.DistricTitle);
+            title.setText(R.string.share);
+            // set logo images
+        }
+
+
+
+
+
+
+        return view;
+    }
+}
---
+++ app/src/main/java/in/technicalkeeda/webview/LandingPage.java
@@ -0,0 +1,46 @@
+package in.technicalkeeda.webview;
+
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.GridView;
+
+public class LandingPage extends AppCompatActivity {
+    GridView simpleGrid;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        setContentView(R.layout.activity_landing_page);
+
+
+
+
+        final int logos[] = {R.drawable.uttarakhand72,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher
+                ,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher
+                ,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_launcher,R.drawable.ic_share};
+        Resources res = getResources();
+       // String[] myStrings = res.getStringArray(R.array.birthdaywishes);
+        simpleGrid = (GridView) findViewById(R.id.simpleGridView); // init GridView
+        // Create an object of CustomAdapter and set Adapter to GirdView
+        CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), logos);
+        simpleGrid.setAdapter(customAdapter);
+        // implement setOnItemClickListener event on GridView
+        simpleGrid.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // set an Intent to Another Activity
+                Intent intent = new Intent(LandingPage.this, MainActivity.class);
+                intent.putExtra("image", logos[position]); // put image data in Intent
+                intent.putExtra("code", position);
+                startActivity(intent); // start Intent
+            }
+        });
+    }
+}
