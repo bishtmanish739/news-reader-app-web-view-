e94ecb2044a204adbc74bc5389857c6a50976dd2
Mahavir Singh <44024089+bishtmanish739@users.noreply.github.com>
2020-09-30 10:36:50 +0530

    update 1.0.1

--- app/src/main/java/in/technicalkeeda/uttarakhand360news/LandingPage.java
+++ app/src/main/java/in/technicalkeeda/uttarakhand360news/LandingPage.java
@@ -1,10 +1,21 @@
 package in.technicalkeeda.uttarakhand360news;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
+import android.app.AlarmManager;
+import android.app.Notification;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
 import android.content.res.Resources;
+import android.graphics.drawable.BitmapDrawable;
+import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.GridView;
@@ -22,6 +33,8 @@ import java.util.Arrays;
 import java.util.List;
 
 public class LandingPage extends AppCompatActivity {
+    public static final String NOTIFICATION_CHANNEL_ID = "10001" ;
+    private final static String default_notification_channel_id = "default" ;
     GridView simpleGrid;
     List<String> testDeviceIds = Arrays.asList("8AF74256D8D5C1E9EF7F4C87D703027E");
     private InterstitialAd mInterstitialAd;
@@ -49,11 +62,13 @@ public class LandingPage extends AppCompatActivity {
         MobileAds.setRequestConfiguration(configuration);
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
+        //notificationshow();
 
         Resources res = getResources();
         // String[] myStrings = res.getStringArray(R.array.birthdaywishes);
         simpleGrid = (GridView) findViewById(R.id.simpleGridView); // init GridView
         // Create an object of CustomAdapter and set Adapter to GirdView
+        scheduleNotification(getNotification( "Read Now..." ) , 1000*24*60*60 ) ;
         CustomAdapter customAdapter = new CustomAdapter(getApplicationContext(), logos);
         simpleGrid.setAdapter(customAdapter);
         mInterstitialAd=new InterstitialAd(this);
@@ -79,7 +94,7 @@ public class LandingPage extends AppCompatActivity {
                             Intent shareIntent = new Intent(Intent.ACTION_SEND);
                             shareIntent.setType("text/plain");
                             shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Uttarakhand 360 News");
-                            String shareMessage= "\nLet me recommend you this application Read All News Related To Uttarakand and your Locality\n\n";
+                            String shareMessage= getResources().getString(R.string.sharemgs);
                             shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
                             shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
                             startActivity(Intent.createChooser(shareIntent, "choose one"));
@@ -116,8 +131,8 @@ public class LandingPage extends AppCompatActivity {
                         Intent shareIntent = new Intent(Intent.ACTION_SEND);
                         shareIntent.setType("text/plain");
                         shareIntent.putExtra(Intent.EXTRA_SUBJECT, "Uttarakhand 360 News");
-                        String shareMessage= "\nLet me recommend you this application Read All News Related To Uttarakand and your Locality\n\n";
-                        shareMessage = shareMessage + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
+                        String shareMessage= getResources().getString(R.string.sharemgs);
+                        shareMessage = shareMessage+"\n" + "https://play.google.com/store/apps/details?id=" + BuildConfig.APPLICATION_ID +"\n\n";
                         shareIntent.putExtra(Intent.EXTRA_TEXT, shareMessage);
                         startActivity(Intent.createChooser(shareIntent, "choose one"));
                     }
@@ -130,6 +145,40 @@ public class LandingPage extends AppCompatActivity {
 
 
 
+
+    }
+    private void scheduleNotification (Notification notification , long  delay) {
+        Intent notificationIntent = new Intent( this, MyNotificationPublisher. class ) ;
+        notificationIntent.putExtra(MyNotificationPublisher. NOTIFICATION_ID , 1 ) ;
+        notificationIntent.putExtra(MyNotificationPublisher. NOTIFICATION , notification) ;
+        PendingIntent pendingIntent = PendingIntent. getBroadcast ( this, 0 , notificationIntent , PendingIntent. FLAG_UPDATE_CURRENT ) ;
+        long futureInMillis = SystemClock. elapsedRealtime () + delay ;
+        AlarmManager alarmManager = (AlarmManager) getSystemService(Context. ALARM_SERVICE ) ;
+        assert alarmManager != null;
+        alarmManager.set(AlarmManager. ELAPSED_REALTIME_WAKEUP , futureInMillis , pendingIntent) ;
+    }
+    private Notification getNotification (String content) {
+        Intent intent=new Intent(this,LandingPage.class);
+        PendingIntent p=PendingIntent. getActivity ( this, 0 , intent , PendingIntent. FLAG_UPDATE_CURRENT ) ;
+        NotificationCompat.Builder builder = new NotificationCompat.Builder( this, default_notification_channel_id ) ;
+        builder.setContentTitle( "Missing breaking news ?" ) ;
+        builder.setContentText(content) ;
+        builder.setSmallIcon(R.drawable. ic_baseline_notifications_active_24 ) ;
+        builder.setAutoCancel( true ) ;
+        builder.setChannelId( NOTIFICATION_CHANNEL_ID ).setContentIntent(p) ;
+        return builder.build() ;
+    }
+    void  notificationshow(){
+        if(Build.VERSION.SDK_INT>Build.VERSION_CODES.O){
+            NotificationChannel channel=new NotificationChannel("My Notification","My Notification",NotificationManager.IMPORTANCE_DEFAULT);
+            NotificationManager manager=getSystemService(NotificationManager.class);
+            manager.createNotificationChannel(channel);
+        }
+        NotificationCompat.Builder builder=new NotificationCompat.Builder(this,"My Notification").
+                setContentTitle("noti").setContentText("click on me").setSmallIcon(R.drawable.ic_baseline_notifications_active_24).
+                setAutoCancel(true);
+        NotificationManagerCompat managerCompat=NotificationManagerCompat.from(this);
+        managerCompat.notify(999,builder.build());
 
     }
 
